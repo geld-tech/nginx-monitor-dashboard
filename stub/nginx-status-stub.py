f832750d9836db54c1de0a5eee7bef7ecf86a86b
zlig <gdevillard@gmail.com>
2018-07-28 20:39:49 +0100

    Adds stub service to reproduce NGINX during local development

---
+++ stub/nginx-status-stub.py
@@ -0,0 +1,26 @@
+
+#!/usr/bin/env python
+"""
+    Server Dashboard
+    Display server resources usage
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def index():
+    response = '''Active connections: 4 
+server accepts handled requests
+1650 1650 9255 
+Reading: 0 Writing: 1 Waiting: 3'''
+    return response, 200
+
+@app.route("/nginx_status", strict_slashes=False)
+def nginx_status():
+    response = '''Active connections: 4 
+server accepts handled requests
+1650 1650 9255 
+Reading: 0 Writing: 1 Waiting: 3'''
+    return response, 200
