81e6fe5676f19ca58be717b50d33f1ec6ffa219b
zlig <gdevillard@gmail.com>
2018-07-28 20:47:07 +0100

    Fixes Flake* errors

--- stub/nginx-status-stub.py
+++ stub/nginx-status-stub.py
@@ -1,26 +1,27 @@
-
 #!/usr/bin/env python
 """
-    Server Dashboard
-    Display server resources usage
+    NGINX Status Stub
+    Returns sample resources usage
 """
 from flask import Flask
 
 app = Flask(__name__)
 app.debug = True
 
+
 @app.route("/")
 def index():
-    response = '''Active connections: 4 
+    response = '''Active connections: 4
 server accepts handled requests
-1650 1650 9255 
+1650 1650 9255
 Reading: 0 Writing: 1 Waiting: 3'''
     return response, 200
 
+
 @app.route("/nginx_status", strict_slashes=False)
 def nginx_status():
-    response = '''Active connections: 4 
+    response = '''Active connections: 4
 server accepts handled requests
-1650 1650 9255 
+1650 1650 9255
 Reading: 0 Writing: 1 Waiting: 3'''
     return response, 200
