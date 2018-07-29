769a5c2d971deb3468e8b435de9f69ffaadedf92
zlig <gdevillard@gmail.com>
2018-07-29 22:34:35 +0100

    Adds stub nginx status service capabilities to run

--- stub/nginx-status-stub.py
+++ stub/nginx-status-stub.py
@@ -3,11 +3,17 @@
     NGINX Status Stub
     Returns sample resources usage
 """
+import logging
+import logging.handlers
+from optparse import OptionParser
 from flask import Flask
 
 app = Flask(__name__)
 app.debug = True
 
+# Initialisation
+logging.basicConfig(format='[%(asctime)-15s] [%(threadName)s] %(levelname)s %(message)s', level=logging.INFO)
+logger = logging.getLogger('root')
 
 @app.route("/")
 def index():
@@ -25,3 +31,16 @@ server accepts handled requests
 1650 1650 9255
 Reading: 0 Writing: 1 Waiting: 3'''
     return response, 200
+
+
+if __name__ == "__main__":
+    # Parse options
+    opts_parser = OptionParser()
+    opts_parser.add_option('--port', type="int", dest='port', help='IP Port to listen to.', default=8000)
+    opts_parser.add_option('--debug', action='store_true', dest='debug', help='Print verbose output.', default=False)
+    options, args = opts_parser.parse_args()
+    if options.debug:
+        logger.setLevel(logging.DEBUG)
+        logger.debug('Enabled DEBUG logging level.')
+    logger.info('Options parsed')
+    app.run(host='0.0.0.0', port=options.port)
