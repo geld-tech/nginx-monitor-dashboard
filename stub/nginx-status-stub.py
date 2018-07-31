23cf209b89c23d6a8e04cea29f51246f9df67a53
zlig <gdevillard@gmail.com>
2018-07-31 10:55:09 +0100

    Remove duplicate function

--- stub/nginx-status-stub.py
+++ stub/nginx-status-stub.py
@@ -16,14 +16,6 @@ logging.basicConfig(format='[%(asctime)-15s] [%(threadName)s] %(levelname)s %(me
 logger = logging.getLogger('root')
 
 @app.route("/")
-def index():
-    response = '''Active connections: 4
-server accepts handled requests
-1650 1650 9255
-Reading: 0 Writing: 1 Waiting: 3'''
-    return response, 200
-
-
 @app.route("/nginx_status", strict_slashes=False)
 def nginx_status():
     response = '''Active connections: 4
