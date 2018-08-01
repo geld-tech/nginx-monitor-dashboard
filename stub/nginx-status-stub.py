7404256f61b39ff1fb8fa7b5cecebf1011170138
zlig <gdevillard@gmail.com>
2018-08-01 10:09:49 +0100

    Adds stub for Nginx version

--- stub/nginx-status-stub.py
+++ stub/nginx-status-stub.py
@@ -25,6 +25,23 @@ Reading: 0 Writing: 1 Waiting: 3'''
     return response, 200
 
 
+@app.route("/v")
+@app.route("/version", strict_slashes=False)
+def version():
+    response = 'nginx version: nginx/1.10.3 (Ubuntu)'
+    return response, 200
+
+
+@app.route("/version_full", strict_slashes=False)
+@app.route("/version/full", strict_slashes=False)
+def full_version():
+    response = '''nginx version: nginx/1.10.3 (Ubuntu)
+built with OpenSSL 1.0.2g  1 Mar 2016
+TLS SNI support enabled
+configure arguments: --with-cc-opt='-g -O2 -fPIE -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2' --with-ld-opt='-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now' --prefix=/usr/share/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --lock-path=/var/lock/nginx.lock --pid-path=/run/nginx.pid --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --with-debug --with-pcre-jit --with-ipv6 --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module --with-http_auth_request_module --with-http_addition_module --with-http_dav_module --with-http_geoip_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_image_filter_module --with-http_v2_module --with-http_sub_module --with-http_xslt_module --with-stream --with-stream_ssl_module --with-mail --with-mail_ssl_module --with-threads'''
+    return response, 200
+
+
 if __name__ == "__main__":
     # Parse options
     opts_parser = OptionParser()
